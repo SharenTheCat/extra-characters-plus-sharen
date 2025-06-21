c3dd4bcb0f1344a4897254d89debea4c3a037b00
xLuigiGamerx <88401287+xLuigiGamerx@users.noreply.github.com>
2025-06-21 23:38:36 +0300

    Added Donkey Kong's head switch and fixed some wrong action names for wapeach

---
+++ z-custom-geo-functions.lua
@@ -0,0 +1,58 @@
+-- Custom Geo Functions --
+
+-- Sonic Spin/Ball Acts --
+
+local sSonicSpinBallActs = {
+    [ACT_SPIN_JUMP]        = true,
+    [ACT_SPIN_DASH]        = true,
+    [ACT_SPIN_DASH_CHARGE] = true,
+}
+
+--- @param n GraphNode | FnGraphNode
+--- Switches between the spin and ball models during a spin/ball actions
+function geo_ball_switch(n)
+    local switch = cast_graph_node(n)
+    local m = geo_get_mario_state()
+    if sSonicSpinBallActs[m.action] then
+        switch.selectedCase = ((m.actionTimer - 1) % 4 // 2 + 1)
+    else
+        switch.selectedCase = 0
+    end
+end
+
+-- Wapeach Axe Acts --
+
+local sWapeachAxeActs = {
+    [ACT_AXECHOP]      = true,
+    [ACT_AXESPIN]      = true,
+    [ACT_AXESPINAIR]   = true,
+    [ACT_AXESPINDIZZY] = true,
+}
+
+--- @param n GraphNode | FnGraphNode
+--- Switches between normal hands and axe hands during axe actions
+function geo_custom_hand_switch(n)
+    local switch = cast_graph_node(n)
+    local m = geo_get_mario_state()
+    if sWapeachAxeActs[m.action] then
+        switch.selectedCase = 1
+    else
+        switch.selectedCase = 0
+    end
+end
+
+-- Donkey Kong Angry Acts --
+
+local sDonkeyKongAngryActs = {}
+
+--- @param n GraphNode | FnGraphNode
+--- Switches between normal head and angry head during angry actions
+function geo_custom_dk_head_switch(n)
+    local switch = cast_graph_node(n)
+    local m = geo_get_mario_state()
+    if sDonkeyKongAngryActs[m.action] then
+        switch.selectedCase = 1
+    else
+        switch.selectedCase = 0
+    end
+end
